f3b9f73751467b7f72c9928839d03ecf71640549
Devin Abbott <devinabbott@gmail.com>
2016-08-17 10:28:09 -0700

    Use source maps in dev webpack config

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack')
 
 const options = require('./webpack.config.js')
 
+options.devtool = "source-map"
+
 options.plugins.unshift(
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('development')
