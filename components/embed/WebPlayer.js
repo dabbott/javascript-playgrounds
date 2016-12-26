79ebca44522b369d826ede1a64f520ea87e020d5
Devin Abbott <devinabbott@gmail.com>
2016-12-26 13:36:13 -0800

    Create React component wrapper and dist build

---
+++ components/embed/WebPlayer.js
@@ -0,0 +1,124 @@
+import React, { Component, PropTypes } from 'react'
+
+const paramSchema = {
+
+  // Plain text
+  title: 'text',
+  transpilerTitle: 'text',
+  code: 'text',
+  entry: 'text',
+  initialTab: 'text',
+  platform: 'text',
+  fullscreen: 'text',
+  width: 'text',
+  scale: 'text',
+  assetRoot: 'text',
+
+  // JSON-encoded
+  files: 'json',
+  vendorComponents: 'json',
+  panes: 'json',
+  styles: 'json',
+}
+
+const createUrlParams = (params) => {
+  return Object.keys(params).map(key => {
+    return `${key}=${encodeURIComponent(params[key])}`
+  }).join('&')
+}
+
+const encodeParams = (params) => {
+  return Object.keys(params).reduce((acc, key) => {
+    const value = params[key]
+
+    if (typeof value === 'undefined') return acc
+
+    acc[key] = paramSchema[key] === 'json'
+      ? JSON.stringify(value)
+      : value
+
+    return acc
+  }, {})
+}
+
+const WEB_PLAYER_VERSION = '1.8.1'
+const WEB_PLAYER_URL = `//cdn.rawgit.com/dabbott/react-native-web-player/gh-v${WEB_PLAYER_VERSION}/index.html`
+
+const styles = {
+  iframe: {
+    width: '100%',
+    height: '100%',
+  },
+}
+
+/**
+ * A React component wrapper for the embeddable iframe player. This ensures
+ * properties are passed and encoded correctly.
+ *
+ * Most props are passed directly through to the player; props passed into the
+ * player can't be changed after the initial render. Other props can be updated
+ * normally.
+ */
+export default class WebPlayer extends Component {
+
+  static propTypes = {
+    style: PropTypes.any,
+    baseURL: PropTypes.string,
+
+    // Passthrough
+    title: PropTypes.string,
+    transpilerTitle: PropTypes.string,
+    code: PropTypes.string,
+    entry: PropTypes.string,
+    initialTab: PropTypes.string,
+    platform: PropTypes.string,
+    fullscreen: PropTypes.bool,
+    width: PropTypes.number,
+    scale: PropTypes.number,
+    assetRoot: PropTypes.string,
+    files: PropTypes.array,
+    vendorComponents: PropTypes.array,
+    panes: PropTypes.array,
+    styles: PropTypes.object,
+  }
+
+  static defaultProps = {
+    baseURL: WEB_PLAYER_URL,
+  }
+
+  constructor(props) {
+    super()
+
+    const params = {...props}
+    delete params.style
+    delete params.baseURL
+
+    const encodedParams = encodeParams(params)
+    const hash = '#' + createUrlParams(encodedParams)
+
+    this.hash = hash
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (
+      this.props.style !== nextProps.style ||
+      this.props.baseURL !== nextProps.baseURL
+    )
+  }
+
+  render() {
+    const {hash} = this
+    const {style, baseURL} = this.props
+
+    return (
+      <div style={style}>
+        <iframe
+          style={styles.iframe}
+          frameBorder={0}
+          allowFullScreen
+          src={`${baseURL}${hash}`}
+        />
+      </div>
+    )
+  }
+}
---
+++ webpack/webpack-embed.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const DIRECTORY = path.dirname(__dirname)
+
+module.exports = {
+  entry: path.join(DIRECTORY, 'components', 'embed', 'WebPlayer.js'),
+  output: {
+    path: path.join(DIRECTORY, 'dist'),
+    filename: 'react-native-web-player.js',
+    library: 'react-native-web-player',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { cacheDirectory: true }
+      },
+    ]
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false,
+      },
+      compress: {
+        dead_code: true,
+        drop_console: true,
+        screw_ie8: true,
+        warnings: true,
+      }
+    })
+  ],
+}
