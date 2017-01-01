dcc05f30fbcec1082b4f2fca2cb2db1b15218ec3
Devin Abbott <devinabbott@gmail.com>
2017-01-01 12:46:45 -0800

    Test files

---
+++ examples/framer-test.js
@@ -0,0 +1,14 @@
+# Background layer 
+bg = new BackgroundLayer
+    backgroundColor: "#28affa"
+ 
+# Create a layer 
+layerA = new Layer
+    x: 0
+    y: 0
+    width: 100
+    height: 100
+    backgroundColor: "#fff"
+    
+layerA.center()
+
---
+++ examples/react-native-test.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+})
+
+AppRegistry.registerComponent('App', () => App)
+
