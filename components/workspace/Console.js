06bae7b5decf15c9416e33edd39b241def63a8e0
Devin Abbott <devinabbott@gmail.com>
2017-06-05 06:43:57 -0700

    Move inspector back into Console file to fix bundle splitting

--- components/workspace/Console.js
+++ components/workspace/Console.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
+import Loadable from 'react-loadable'
 
-import Inspector from './Inspector'
 import { prefix, prefixObject } from '../../utils/PrefixInlineStyles'
 
 const styles = prefixObject({
@@ -40,6 +40,27 @@ const styles = prefixObject({
   itemSpacer: {
     width: 8,
   },
+})
+
+const createTheme = (base) => ({
+  ...base,
+  BASE_FONT_SIZE: '13px',
+  TREENODE_FONT_SIZE: '13px',
+  BASE_LINE_HEIGHT: '20px',
+  TREENODE_LINE_HEIGHT: '20px',
+  BASE_BACKGROUND_COLOR: 'transparent',
+})
+
+const Inspector = Loadable({
+  loader: () => import('react-inspector')
+    .then(({default: Inspector, chromeLight}) => {
+      const theme = createTheme(chromeLight)
+
+      console.log("Inspector", Inspector, chromeLight)
+
+      return (props) => <Inspector {...props} theme={theme} />
+    }),
+  LoadingComponent: () => null,
 })
 
 @pureRender
