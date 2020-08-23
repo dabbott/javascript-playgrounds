3da8ef6520126fcc27cfe8f1a11828fb4fd87c98
Devin Abbott <devinabbott@gmail.com>
2020-08-23 12:44:15 -0700

    Bump version

--- src/components/embed/WebPlayer.js
+++ src/components/embed/WebPlayer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import PropTypes from 'prop-types'
 
-const WEB_PLAYER_URL = `https://unpkg.com/react-native-web-player@2.0.0-alpha.23/public/index.html`
+const WEB_PLAYER_URL = `https://unpkg.com/react-native-web-player@2.0.0-alpha.24/public/index.html`
 
 const paramSchema = {
   // Plain text
